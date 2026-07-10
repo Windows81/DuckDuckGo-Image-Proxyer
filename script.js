b731ea1a8b686d6963d174f6e81c3135a233ac97
VisualPlugin <aboshadi.n.ahmed@gmail.com>
2026-07-10 02:45:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,36 +4,36 @@
 // @version      0.1
 // @description  Replace all images in a page with proxied versions
 // @author       VisualPlugin
-// @match        https://www.google.com/search?*
+// @run-at       document-start
+// @match        *://*/*
 // @grant        none
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
- 
-    function replaceImages() {
-        const observerCallback = (mutationsList, observer) => {
 
-          // Loops through each mutation record.
-          for (const mutation of mutationsList) {
+    const replaceImages = (mutationsList, observer) => {
+
+        // Loops through each mutation record.
+        console.log(mutationsList);
+        for (const mutation of mutationsList) {
 
             if (mutation.type !== 'childList') continue;
 
             // Processes added nodes.
             mutation.addedNodes.forEach(img => {
 
-              if (img.nodeType !== Node.ELEMENT_NODE) return;
-              if (img.tagName !== "IMG") return;
-              if (!img.dataset.originalSrc) img.dataset.originalSrc = img.src;
-              let replacementUrl = 'https://external-content.duckduckgo.com/iu/?w=200&u=' + encodeURIComponent(img.dataset.originalSrc);
-              img.src = replacementUrl;
-              img.srcset = "";
+                if (img.nodeType !== Node.ELEMENT_NODE) return;
+                if (img.tagName !== "IMG") return;
+                if (!img.dataset.originalSrc) img.dataset.originalSrc = img.src;
+                let replacementUrl = 'https://external-content.duckduckgo.com/iu/?w=200&u=' + encodeURIComponent(img.dataset.originalSrc);
+                img.src = replacementUrl;
+                img.srcset = "";
 
             });
-          }
-        };
-    }
- 
+        }
+    };
+
     // Keeps replacing images as more load.
     const observer = new MutationObserver(replaceImages);
     observer.observe(document.body, { childList: true, subtree: true });
